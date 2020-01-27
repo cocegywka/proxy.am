3b4d71f9f32042b7e84ca106b3094a5f35094286
Azvukov <cocegywka@gmail.com>
2020-01-27 13:34:58 +0200

    banner price box markup

--- components/promo-carousel.vue
+++ components/promo-carousel.vue
@@ -22,6 +22,18 @@
             <strong>УВЕЛИЧЕНИЕ ОНЛАЙНА</strong>
             <p>20,000 IP всего за 10$ в месяц.</p>
           </div>
+          <div class="promo-price row">
+            <div class="col-sm-6">
+              <span class="shared">SHARED PROXY</span>
+              <span class="ip">20 000 IP</span>
+            </div>
+            <div class="col-sm-6">
+              <div class="price">
+                <strong>10<sup>$</sup></strong>
+                <em>в месяц.</em>
+              </div>
+            </div>
+          </div>
         </slide>
       </carousel>
     </div>
@@ -66,23 +78,75 @@
       }
       .button{
         display: inline-block;
-        width: 177px;
-        height: 40px;
-        font-size: 13px;
+        width: 197px;
+        height: 35px;
+        font-size: 14px;
         text-align: center;
-        line-height: 37px;
+        line-height: 35px;
         display: block;
         border-radius: 50px;
         padding: 0;
         text-decoration: none;
-        background: #185c93;
+        background-color: #0258b9;
         color: #fff;
-        margin: 0 0 0;
+        margin: 0 auto;
         position: relative;
-        border: 3px solid #185c93;
+        border: 1px solid #0258b9;
         &:hover{
           transition: all 0.3s ease;
-          background: #0080ec;
+          background-color:#2c3760;
+          color: #3175ac;
+        }
+      }
+    }
+    .promo-price{
+      position: absolute;
+      top: 216px;
+      right: 40px;
+      font-family: 'neogreymedium';
+      text-align: left;
+      width: 333px;
+
+      .shared{
+        display: block;
+        color: #a9b1da;
+        font-size: 12px;
+        padding: 0 0 25px;
+      }
+      .ip{
+        display: block;
+        color: #a9b1da;
+        font-size: 16px;
+        padding: 0 0 0 3px;
+      }
+      .price{
+        width: 92px;
+        height: 92px;
+        margin-top: -15px;
+        float: right;
+        border-radius: 100%;
+        background: rgb(0,131,236);
+        background: -moz-linear-gradient(180deg, rgba(0,131,236,1) 0%, rgba(0,167,235,1) 100%);
+        background: -webkit-linear-gradient(180deg, rgba(0,131,236,1) 0%, rgba(0,167,235,1) 100%);
+        background: linear-gradient(180deg, rgba(0,131,236,1) 0%, rgba(0,167,235,1) 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#0083ec",endColorstr="#00a7eb",GradientType=1);
+        text-align: center;
+        strong{
+          color: #fff;
+          display: block;
+          font-size: 45px;
+          line-height: 45px;
+          padding: 17px 0 0 7px;
+          sup{
+            top: -22px;
+            font-size: 15px;
+          }
+        }
+        em{
+          color: #b9bfe7;
+          text-align: center;
+          display: block;
+          font: 13px/16px 'Rubik';
         }
       }
     }
